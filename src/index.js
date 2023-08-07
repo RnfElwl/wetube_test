bcc72743899035890686dae70fc7ed8914ccec7d
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-08-07 17:30:17 +0900

    controller and response test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import express from "express";
 
 const app = express();
 const PORT = 4000;
-console.log("start!");
+
+const middeleware = (req, res, next) => {
+  console.log("middle");
+  next();
+};
+const handleHome = (req, res) => {
+  return res.send("hello");
+};
+
+app.get("/", middeleware, handleHome);
 
 const handleLIstening = () => console.log("Server listenting");
 
