a8fe1da089fa5477724b53295fc09f2929255855
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-08-30 23:28:19 +0900

    퍼그 변수 테스트

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,6 +1,6 @@
-const trending = (req, res) => res.render("home");
-const see = (req, res) => res.render("watch");
-const edit = (req, res) => res.send("Edit");
+const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 const search = (req, res) => res.send("Search");
 const deleteVideo = (req, res) => res.send("Delete Video");
 const upload = (req, res) => res.send("Upload");
