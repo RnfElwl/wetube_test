402751160fbf14b07fb03cfb25b99df965477cc4
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-09-01 23:49:41 +0900

    퍼그에서  배열 다루기

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -3,8 +3,10 @@ const fakeUser = {
   loggedIn: false,
 };
 
-const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", fakeUser: fakeUser, videos });
+};
 const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 const search = (req, res) => res.send("Search");
