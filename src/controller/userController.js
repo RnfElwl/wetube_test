9a1ef005710c89b8159ef04fbe483d11fc526534
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-09-27 00:10:10 +0900

    비번확인 이메일 아이디 중복

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,7 +2,22 @@ import User from "../models/User";
 
 const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({ name, username, email, password, location });
   return res.redirect("/login");
 };
